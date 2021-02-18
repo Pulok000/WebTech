e9f0a5d438e9c38f7b3f8d5732d47bc2adf6c333
Pulok aka poka <pulok19524@gmail.com>
2021-02-18 18:00:31 +0600

    lab task 02:->1 commpleted:6th commit

--- Lab task 03/1/1.php
+++ Lab task 03/1/1.php
@@ -22,31 +22,34 @@
 
 
 	<?php
-//got helped from: https://www.coding.academy/blog/how-to-use-regular-expressions-to-check-password-strength
+//got helped from/Notes: https://www.coding.academy/blog/how-to-use-regular-expressions-to-check-password-strength
 
 	$flag=TRUE;
 	$name=$pass=$errname=$errpass="";
 
-	$checkName=$_POST["fname"];
-	$checkPass=$_POST["fpass"];
+
 	//$nSize=strlen($_POST["fname"]);
 	//$pSize=strlen($_POST["fpass"]);
 
 	//$namePatt1= '/^(?=.*[A-Z])$/';
 
-	$namePatt2='/^.{2,}$/';
-	$passPatt1='/^.{8,}$/';
-	$passPatt2='/^(?=.*[@#$%]).{8,}$/';
+
 
 
 	if($_SERVER["REQUEST_METHOD"]=="POST")
 	{
-	
+	$checkName=$_POST["fname"];
+	$checkPass=$_POST["fpass"];
+
+	$namePatt2='/^.{2,}$/';
+	$passPatt1='/^.{8,}$/';
+	$passPatt2='/^(?=.*[@#$%]).{8,}$/';
+
 		if(!empty($checkName))
 		{
 			if(!preg_match("/^[a-zA-Z1-9-._ ]*$/",$checkName))
 			{
-				$errname="* User Namecan contain alpha numeric characters, period,dash or underscore only";
+				$errname="* User Name can contain alpha numeric characters, period,dash or underscore only";
 				$flag=false;
 			}
 			else if(!preg_match($namePatt2,$checkName))
@@ -64,7 +67,7 @@
 
 		if(!empty($checkPass))
 		{
-			if(!preg_match($passPatt1,$checkName))
+			if(!preg_match($passPatt1,$checkPass))
 			{
 				$errpass="Password must not be less than eight (8) characters";
 			}
@@ -85,60 +88,6 @@
 
 	}
 
-/*
-	if($_SERVER["REQUEST_METHOD"]=="POST")
-	{
-
-		
-		$j=str_word_count($check,0);
-
-
-		if(!isset($_POST["fname"]))
-		{
-			$errname="* Name is required";
-			$flag=FALSE;
-		}
-		else if($j<2)
-		{
-
-
-
-			else if(!preg_match("/^[a-zA-Z1-9-._ ]*$/",$check))
-			{
-
-				$errname = "You can use a-z, A-Z, period,dash only";
-				$flag=FALSE;
-			}
-			else
-			{
-				$errname="* Name should contain  at least two words";
-				$flag=FALSE;
-			}
-
-
-		}
-		else
-		{
-
-			if(!preg_match("/^[a-zA-Z1-9-. ]*$/",$check))
-			{
-
-				$errname = "You can use a-z, A-Z, period,dash only";
-				$flag=FALSE;
-			}
-			else
-			{
-				$name=test_input($_POST["fname"]);
-			}
-
-
-			
-
-		}
-
-
-	}
-*/
 
 
 	function test_input($data) {
@@ -157,14 +106,20 @@
 	<legend><h3>LOGIN</h3></legend> 
 
 	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> ">
-		<!-- <label for="name">User Name :</label> -->
+		<label for="name">User Name :</label> 
 		<input type="text" name="fname">
 		<span class="error"><?php echo $errname;?></span><br>
-		<!-- <label for="fpassword">Password :</label> -->
+		<label for="fpassword">Password :</label> 
 		<input type="text" name="fpass">
 		<span class="error"><?php echo $errpass;?></span> 
 		<hr align=center  size=1>
+
+		<label for="fremember">Remember me</label> 
+		<input type="checkbox" name="frem">
+		<br>
+		<br>
 		<input type="submit" name="fsubmit" value="Submit">
+		<a href="forgetPass.php">Forget password?</a>
 	</form>
 
 </fieldset>
