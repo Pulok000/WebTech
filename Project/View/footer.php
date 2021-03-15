f34e2fb34a3f9583dad8988006a98fd0928a18a7
PULOK AHMED <pulok19524@gmail.com>
2021-03-15 13:29:14 +0600

    Lab task 04 : New Update(fixing link redirect error): 6th commit

--- Project/View/footer.php
+++ Project/View/footer.php
@@ -5,25 +5,6 @@
 		<title></title>
 		<link rel="stylesheet" type="text/css" href="style2.css">
 
-		<!-- <style type="text/css">
-			
-	#footerdiv{ 
-	width: 800px; 
-	bottom: 0px; 
-
-	position: absolute; 
-	padding-top:10px; 
-	padding-bottom:10px; 
-	text-align:center; 
-	font-size:20px; 
-
-	border-style: solid;
-    border-width: thin;
-	} 
-
-
-
-		</style> -->
 
 
 	</head>
